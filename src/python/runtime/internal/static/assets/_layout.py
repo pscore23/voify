b48e7f6edc4a6f48a46ca7c9f005b7a7689b26f7
pscore23 <rubik2357x@gmail.com>
2022-10-01 22:34:31 +0900

    細かな変更

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -4,10 +4,11 @@ import subprocess
 
 import PySimpleGUI as sg
 
-sg.theme("Dark Black")
+sg.theme("DarkBlue3")
 
 # autopep8: off
 LAYOUT: list = [
-    [[sg.Text(lib)] for lib in [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]]
+    [sg.Text("インストール済みの pip ライブラリ:")],
+    [[sg.Text(lib, text_color="Black", background_color="White")] for lib in [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]]
 ]
 # autopep8: on
