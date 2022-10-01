df52ef7346cdf446c33eb12912608ebc3c110694
pscore23 <rubik2357x@gmail.com>
2022-10-01 16:38:13 +0900

    Type Hint の変更など

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -1,11 +1,14 @@
+from __future__ import absolute_import
+
 import subprocess
+from typing import Any
 
 import PySimpleGUI as sg
 
 sg.theme("Dark Black")
 
 # autopep8: off
-LAYOUT: list = [
+LAYOUT: list[Any] = [
     [[sg.Text(lib)] for lib in [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]]
 ]
 # autopep8: on
