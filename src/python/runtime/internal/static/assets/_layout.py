053207a137fcc19f13e023f2f2d6e77ced3bfae0
pscore23 <rubik2357x@gmail.com>
2022-10-01 16:21:58 +0900

    Initial commit

---
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -0,0 +1,11 @@
+import subprocess
+
+import PySimpleGUI as sg
+
+sg.theme("Dark Black")
+
+# autopep8: off
+LAYOUT: list = [
+    [[sg.Text(lib)] for lib in [name.split("==")[0] for name in subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")]]
+]
+# autopep8: on
