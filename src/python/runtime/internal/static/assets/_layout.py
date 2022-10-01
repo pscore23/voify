f4ea161bed75a5dce6ab829e55425b6bc3736d92
pscore23 <rubik2357x@gmail.com>
2022-10-01 23:32:44 +0900

    変更

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -1,14 +1,13 @@
-from __future__ import absolute_import
-
-import subprocess
-
 import PySimpleGUI as sg
+
+from internal.static._common_func import _get_all_lib
 
 sg.theme("DarkBlue3")
 
 # autopep8: off
 LAYOUT: list = [
     [sg.Text("インストール済みの pip ライブラリ:")],
-    [[sg.Text(lib, text_color="Black", background_color="White")] for lib in [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]]
+    [[sg.Text(lib, text_color="Black", background_color="White")] for lib in _get_all_lib()],
+    [sg.Button("情報を更新する...", key="-UPDATE-")]
 ]
 # autopep8: on
