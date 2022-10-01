899b743392c972b4403d9de6fa06e164dd0d7c40
pscore23 <rubik2357x@gmail.com>
2022-10-01 16:46:43 +0900

    細かな変更

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
 
 import subprocess
-from typing import Any
 
 import PySimpleGUI as sg
 
 sg.theme("Dark Black")
 
 # autopep8: off
-LAYOUT: list[Any] = [
+LAYOUT: list = [
     [[sg.Text(lib)] for lib in [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]]
 ]
 # autopep8: on
