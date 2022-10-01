3399170fba87377d40e8165dd7290797f220e4c9
pscore23 <rubik2357x@gmail.com>
2022-10-01 23:32:16 +0900

    System クラスの追加など

--- src/python/runtime/main.py
+++ src/python/runtime/main.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
 import gc
+import io
+import os
 import sys
 
 import PySimpleGUI as sg
@@ -9,14 +11,17 @@ from PySimpleGUI import Window
 from internal.static.assets._layout import LAYOUT
 
 
-class MainProcess:
+class MainProcess(object):
     """メインプロセスのクラス:
             - GUI 処理
             - pip の管理
     """
 
     def __init__(self) -> None:
+        self._system: System = System()
         self._window: Window = sg.Window("voify", LAYOUT)
+
+        self._system._setup()
 
     def run(self) -> None:
         """全ての処理を開始 (または終了) する:
@@ -29,8 +34,32 @@ class MainProcess:
             if event in (sg.WIN_CLOSED,):
                 break
 
+            if event == "-UPDATE-":
+                self._system.restart()
+
+            self._window.refresh()
+
         self._window.close()
-        sys.exit(gc.collect())
+        self._system._cleanup()
+        sys.exit()
+
+
+class System(object):
+    @staticmethod
+    def _setup():
+        sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
+        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+        sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
+
+    @staticmethod
+    def _cleanup():
+        sys.stdout.flush()
+        gc.collect()
+
+    def restart(self):
+        self._cleanup()
+
+        os.execv(sys.executable, ["python"] + sys.argv)
 
 
 if __name__ == "__main__":
