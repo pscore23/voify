05d80199467a08c721a06eca6a853430af660bb8
pscore23 <rubik2357x@gmail.com>
2022-10-01 16:30:21 +0900

    リストから空文字列を削除

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -6,6 +6,6 @@ sg.theme("Dark Black")
 
 # autopep8: off
 LAYOUT: list = [
-    [[sg.Text(lib)] for lib in [name.split("==")[0] for name in subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")]]
+    [[sg.Text(lib)] for lib in [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]]
 ]
 # autopep8: on
