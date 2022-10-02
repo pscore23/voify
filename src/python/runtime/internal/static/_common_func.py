3fedbb8e0fd4c5b7664edf93628d7ca689065358
pscore23 <rubik2357x@gmail.com>
2022-10-02 21:12:13 +0900

    deleted

--- src/python/runtime/internal/static/_common_func.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-import subprocess
-
-
-def _get_all_lib():
-    return [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]
