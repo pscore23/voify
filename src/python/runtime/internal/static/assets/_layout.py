ad860d4d0c99909aea6c769960af1c0042822663
pscore23 <rubik2357x@gmail.com>
2022-10-02 20:56:55 +0900

    細かな変更

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -10,6 +10,6 @@ sg.theme("DarkBlue3")
 LAYOUT: list = [
     [sg.Text("インストール済みの pip ライブラリ:")],
     [[sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in _get_all_lib()],
-    [sg.Button("情報を更新する...", key="-UPDATE-")]
+    [sg.Button("再起動する...", key="-RESTART-")]
 ]
 # autopep8: on
