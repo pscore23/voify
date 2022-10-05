9ce9ec0b0e5e287ea2d89bd09853788561195040
pscore23 <rubik2357x@gmail.com>
2022-10-05 21:09:40 +0900

    Type Hint の追加

--- src/python/runtime/internal/_common_func.py
+++ src/python/runtime/internal/_common_func.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 import subprocess
 
 
-def _get_all_lib():
+def _get_all_lib() -> list[str]:
     return [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]
