047852744fc5a4990f05925697a7f7523a3f1e52
pscore23 <rubik2357x@gmail.com>
2022-10-05 21:09:58 +0900

    Type Hint の追加

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import PySimpleGUI as sg
+from PySimpleGUI import Button, Combo, Input, Text
 
 from internal._common_func import _get_all_lib
 
@@ -9,10 +10,10 @@ sg.theme("DarkBlue3")
 _PAD: tuple[tuple[int, int], tuple[int, int]] = ((5, 0), (30, 40))
 
 # autopep8: off
-LAYOUT: list = [
+LAYOUT: list[list[Text] | list[list[Input]] | list[Input | Text | Combo | Button] | list[Button]] = [
     [sg.Text("インストール済みの pip ライブラリ:")],
     [[sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in _get_all_lib()],
-    [sg.Input(pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["インストール", "アンインストール"], "インストール", size=(16, None), pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
+    [sg.Input(key="-INPUT-", pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["インストール", "アンインストール"], "インストール", size=(16, None), pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
     [sg.Button("再起動する...", button_color="Red", key="-RESTART-")]
 ]
 # autopep8: on
