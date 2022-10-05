37c71fa3840c49426dc90dc0a3c99234bfa35547
pscore23 <rubik2357x@gmail.com>
2022-10-05 20:39:53 +0900

    Type Hint を追加

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -6,11 +6,13 @@ from internal._common_func import _get_all_lib
 
 sg.theme("DarkBlue3")
 
+PAD: tuple[tuple[int, int], tuple[int, int]] = ((5, 0), (30, 40))
+
 # autopep8: off
 LAYOUT: list = [
     [sg.Text("インストール済みの pip ライブラリ:")],
     [[sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in _get_all_lib()],
-    [sg.Input(pad=((5, 0), (30, 40))), sg.Text("を", pad=((5, 0), (30, 40))), sg.Combo(["インストール", "アンインストール"], "インストール", size=(16, None), pad=((5, 0), (30, 40)), readonly=True), sg.Button("実行", button_color="Gray", pad=((5, 0), (30, 40)), key="-START-")],
+    [sg.Input(pad=PAD), sg.Text("を", pad=PAD), sg.Combo(["インストール", "アンインストール"], "インストール", size=(16, None), pad=PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=PAD, key="-START-")],
     [sg.Button("再起動する...", button_color="Red", key="-RESTART-")]
 ]
 # autopep8: on
