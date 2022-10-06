8484dd4752b68a656244a03ed9855614bf8744d2
pscore23 <rubik2357x@gmail.com>
2022-10-06 18:25:03 +0900

    細かな変更

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -13,7 +13,7 @@ _PAD: tuple[tuple[int, int], tuple[int, int]] = ((5, 0), (30, 40))
 LAYOUT: list[list[Text] | list[list[Input]] | list[Input | Text | Combo | Button] | list[Button]] = [
     [sg.Text("インストール済みの pip ライブラリ:")],
     [[sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in _get_all_lib()],
-    [sg.Input(key="-INPUT-", pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["インストール", "アンインストール"], "インストール", size=(16, None), pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
+    [sg.Input(key="-INPUT-", pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["アップデート", "インストール", "アンインストール"], "アップデート", size=(16, None), key="-SELECT-", pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
     [sg.Button("再起動する...", button_color="Red", key="-RESTART-")]
 ]
 # autopep8: on
