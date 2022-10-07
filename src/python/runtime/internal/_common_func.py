203236bea4718a4700bac4484b939233cafdf70d
pscore23 <rubik2357x@gmail.com>
2022-10-07 22:24:54 +0900

    [add] "_run_pyf_command" 関数を追加

--- src/python/runtime/internal/_common_func.py
+++ src/python/runtime/internal/_common_func.py
@@ -1,18 +1,19 @@
 from __future__ import absolute_import
 
 import subprocess
-
-_UPDATE = [name.split("==")[0] for name in list(filter(None, subprocess.run(
-    "pip list --outdated --format freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]
+from typing import Any
 
 
-def _get_all_lib() -> list[str]:
-    return [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze --all", capture_output=True, text=True, check=True).stdout.split("\n")))]
+def _run_pyf_command(__text: str) -> list[str]:
+    return [name.split("==")[0] for name in list(filter(None, subprocess.run(__text, capture_output=True, text=True, check=True).stdout.split("\n")))]
 
 
-def _require_update(lib) -> bool:
-    if lib in _UPDATE:
-        return True
+def get_all_lib() -> list[str]:
+    return _run_pyf_command("pip freeze --all")
 
-    else:
-        return False
+
+def require_update(lib: Any) -> bool:
+    return bool(lib in _UPDATE)
+
+
+_UPDATE = _run_pyf_command("pip list --outdated --format freeze")
