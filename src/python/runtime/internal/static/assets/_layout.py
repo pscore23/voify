1225fe0e441af1341d06e3af1bc26f5d0dffb2ac
pscore23 <rubik2357x@gmail.com>
2022-10-07 23:04:43 +0900

    [add] #3 の 1 つ目の提案を実装

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 import PySimpleGUI as sg
-from PySimpleGUI import Button, Combo, Input, Text
+from PySimpleGUI import Button, Column, Combo, Input, Text
 
 from internal._common_func import get_all_lib, require_update
 
@@ -10,10 +10,10 @@ sg.theme("DarkBlue3")
 _PAD: tuple[tuple[int, int], tuple[int, int]] = ((5, 0), (30, 40))
 
 # autopep8: off
-LAYOUT: list[list[Text] | list[list[Input]] | list[Input | Text | Combo | Button] | list[Button]] = [
+LAYOUT: list[list[Text] | list[Column] | list[Input | Text | Combo | Button] | list[Button]] = [
     [sg.Text("インストール済みの pip ライブラリ:")],
 
-    [[sg.Input(lib, disabled=True, text_color="Red", background_color="White")] if require_update(lib) else [sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in get_all_lib()],
+    [sg.Column([[sg.Input(lib, disabled=True, text_color="Red", background_color="White")] if require_update(lib) else [sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in get_all_lib()], scrollable=True)],
 
     [sg.Input(key="-INPUT-", pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["アップデート", "インストール", "アンインストール"], "アップデート", size=(16, None), text_color="Green", button_arrow_color="Blue", key="-SELECT-", pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
 
