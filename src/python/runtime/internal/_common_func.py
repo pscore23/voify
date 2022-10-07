d27ff76902d74cf95989546d01f3e444bf14b614
pscore23 <rubik2357x@gmail.com>
2022-10-07 19:59:39 +0900

    [add] "_UPDATE" 定数と "_require_update" 関数を追加

--- src/python/runtime/internal/_common_func.py
+++ src/python/runtime/internal/_common_func.py
@@ -2,6 +2,17 @@ from __future__ import absolute_import
 
 import subprocess
 
+_UPDATE = [name.split("==")[0] for name in list(filter(None, subprocess.run(
+    "pip list --outdated --format freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]
+
 
 def _get_all_lib() -> list[str]:
-    return [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze", capture_output=True, text=True, check=True).stdout.split("\n")))]
+    return [name.split("==")[0] for name in list(filter(None, subprocess.run("pip freeze --all", capture_output=True, text=True, check=True).stdout.split("\n")))]
+
+
+def _require_update(lib) -> bool:
+    if lib in _UPDATE:
+        return True
+
+    else:
+        return False
