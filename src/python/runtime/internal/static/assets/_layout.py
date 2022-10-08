948b207fc4ca38dfac0384d21b618313b576cc2a
pscore23 <rubik2357x@gmail.com>
2022-10-08 16:30:48 +0900

    [add] バージョン表示機能を追加

--- src/python/runtime/internal/static/assets/_layout.py
+++ src/python/runtime/internal/static/assets/_layout.py
@@ -8,17 +8,21 @@ from internal._common_func import get_all_lib, require_update
 sg.theme("DarkBlue3")
 
 _PAD: tuple[tuple[int, int], tuple[int, int]] = ((5, 0), (30, 40))
+_LIB: tuple[list[str], list[str]] = get_all_lib()
 
-# autopep8: off
+
 LAYOUT: list[list[Text] | list[Column] | list[Input | Text | Combo | Button] | list[Button]] = [
     [sg.Text("インストール済みの pip ライブラリ:")],
 
-    [sg.Column([[sg.Input(lib, disabled=True, text_color="Red", background_color="White")] if require_update(lib) else [sg.Input(lib, disabled=True, text_color="Black", background_color="White")] for lib in get_all_lib()], scrollable=True)],
+    [sg.Text("名前 | バージョン".rjust(83, " "))],
 
-    [sg.Input(key="-INPUT-", pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["アップデート", "インストール", "アンインストール"], "アップデート", size=(16, None), text_color="Green", button_arrow_color="Blue", key="-SELECT-", pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
+    [sg.Column([[sg.Input(name, disabled=True, text_color="Orange", background_color="White"), sg.Input(ver, disabled=True, text_color="Orange", background_color="White")] if require_update(name) else [sg.Input(name, disabled=True, text_color="Green",
+               background_color="White"), sg.Input(ver, disabled=True, text_color="Green", background_color="White")] for name, ver in zip(_LIB[0], _LIB[1])], scrollable=True)],
+
+    [sg.Input(key="-INPUT-", pad=_PAD), sg.Text("を", pad=_PAD), sg.Combo(["アップデート", "インストール", "アンインストール"], "アップデート", size=(16, None), text_color="Blue",
+                                                                         button_arrow_color="Blue", key="-SELECT-", pad=_PAD, readonly=True), sg.Button("実行", button_color="Gray", pad=_PAD, key="-START-")],
 
     [sg.Text("入力待ちです...", background_color="Black", key="-OUTPUT-")],
 
     [sg.Button("再起動する...", button_color="Red", key="-RESTART-", pad=_PAD)]
 ]
-# autopep8: on
