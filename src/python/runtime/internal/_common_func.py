8ddb67067d7bd74a9f7f8ddf53764e64b0c8ecc4
pscore23 <rubik2357x@gmail.com>
2022-10-08 16:29:59 +0900

    [fix] "_UPDATE" 定数のバグを修正

--- src/python/runtime/internal/_common_func.py
+++ src/python/runtime/internal/_common_func.py
@@ -4,16 +4,21 @@ import subprocess
 from typing import Any
 
 
-def _run_pyf_command(__text: str) -> list[str]:
-    return [name.split("==")[0] for name in list(filter(None, subprocess.run(__text, capture_output=True, text=True, check=True).stdout.split("\n")))]
+def _run_command(__text: str) -> list[str]:
+    return [line for line in list(filter(None, subprocess.run(__text, capture_output=True, text=True, check=True).stdout.split("\n")))]
 
 
-def get_all_lib() -> list[str]:
-    return _run_pyf_command("pip freeze --all")
+def get_all_lib() -> tuple[list[str], list[str]]:
+    _info = _run_command("pip freeze --all")
+
+    lib_name = [name.split("==")[0] for name in _info]
+    lib_ver = [ver.split("==")[1] for ver in _info]
+
+    return (lib_name, lib_ver)
 
 
 def require_update(lib: Any) -> bool:
     return bool(lib in _UPDATE)
 
 
-_UPDATE = _run_pyf_command("pip list --outdated --format freeze")
+_UPDATE = (lambda: [name.split("==")[0] for name in _run_command("pip list --outdated --format freeze")])()
