1c55f9ccd4286420a40eb1f82fc9268cde41c02c
pscore23 <rubik2357x@gmail.com>
2022-10-09 22:30:57 +0900

    [add] アップデート可能なバージョンを表示

--- src/python/runtime/internal/_common_func.py
+++ src/python/runtime/internal/_common_func.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 
+import json
 import subprocess
 from typing import Any
 
 
 def _run_command(__text: str) -> list[str]:
-    return [line for line in list(filter(None, subprocess.run(__text, capture_output=True, text=True, check=True).stdout.split("\n")))]
+    return [line for line in list(filter(None, subprocess.run(
+        __text, capture_output=True, text=True, check=True).stdout.split("\n")))]
 
 
 def get_all_lib() -> tuple[list[str], list[str]]:
@@ -17,8 +19,14 @@ def get_all_lib() -> tuple[list[str], list[str]]:
     return (lib_name, lib_ver)
 
 
-def require_update(lib: Any) -> bool:
-    return bool(lib in _UPDATE)
+def require_update(lib_name: Any) -> tuple[bool, Any]:
+    _lib_dict = {}
+
+    for i in _UPDATE:
+        _lib_dict |= i
+
+    return (bool(lib_name in _lib_dict.keys()), _lib_dict.get(lib_name))
 
 
-_UPDATE = (lambda: [name.split("==")[0] for name in _run_command("pip list --outdated --format freeze")])()
+_UPDATE: list[dict[Any, Any]] = [{name.get("name"): name.get("latest_version")} for name in json.loads(
+    str(_run_command("pip list --outdated --format json")[0]))]
